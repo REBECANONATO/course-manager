cc33ed392c5f048f3c1c5b6ecf04dd1ddd105caa
REBECANONATO <rebecanonato89@gmail.com>
2021-02-11 17:12:15 -0300

    listagem dos cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 
 @Component({
   selector: "app-course-list",
-  template: '<h2>Course List </h2>'
+  templateUrl: "./course-list.component.html"
 })
 
 
 
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular',
+        imageUrl: '',
+        price: 99.99,
+        code: 'xpss - 4528',
+        duration: 120,
+        rating: 4.5,
+        releaseDate: 'December, 2, 2020'
+      },
+      {
+        id: 2,
+        name: 'Reactjs',
+        imageUrl: '',
+        price: 99.99,
+        code: 'xppp - 4568',
+        duration: 80,
+        rating: 4.9,
+        releaseDate: 'December, 2, 2020'
+      }
+    ];
+  }
 
 }
